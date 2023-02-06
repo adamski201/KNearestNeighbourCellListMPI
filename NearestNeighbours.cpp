177e10d5c3d24c6b92915e22a804647df3aaa204
Adam O'Sullivan <90043609+adamski201@users.noreply.github.com>
2023-02-06 12:00:15 +0000

    final

--- NearestNeighbours.cpp
+++ NearestNeighbours.cpp
@@ -190,6 +190,106 @@ std::vector<int> findNeighboursCellListMPI(std::vector<std::vector<double>>& pos
     return results;
 }
 
+// Function that outputs the number of neighbours for each atom in the input vector given a cutoff distance
+// using an atom-centred cell list approach and load-balanced MPI
+std::vector<int> findNeighboursCellListMPIv2(std::vector<std::vector<double>>& positions, double L, double cutoff, int rank, int num_procs)
+{
+    // Initalise results vector
+    std::vector<int> results(positions.size(), 0);
+
+    // Define cell dimensions
+    double L_cell = cutoff;
+
+    // Define number of cells in each dimension
+    int num_cells = L / L_cell;
+
+    // Create a vector of vectors to store the atoms in each cell
+    std::vector<std::vector<int>> cells(num_cells*num_cells*num_cells);
+
+    // Assign each atom to a cell based on its position
+    for (int i = rank; i < positions.size(); i += num_procs) 
+    {
+        // Calculate cell co-ordinates
+        int x = (int) (positions[i][0] / L_cell);
+        int y = (int) (positions[i][1] / L_cell);
+        int z = (int) (positions[i][2] / L_cell);
+
+        // Handle particles outside of the cube
+        if (x >= num_cells) {x= num_cells-1;}
+        if (y >= num_cells) {y = num_cells-1;}
+        if (z >= num_cells) {z = num_cells-1;}
+        if (x < 0) {x = 0;}
+        if (y < 0) {y = 0;}
+        if (z < 0) {z = 0;}
+
+        // Flattens 3D cell co-ordinates into a linear index
+        int cell_index = x + y * num_cells + z * num_cells * num_cells;
+
+        // Populates the cells with their atoms.
+        cells[cell_index].push_back(i);
+    }
+
+    // Loop over all atoms
+    for (int i = 0; i < positions.size(); ++i)
+    {
+        // Find position of atom
+        std::vector<double> atom1 = positions[i];
+
+        // Calculate cell index
+        int x = (int) (atom1[0] / L_cell);
+        int y = (int) (atom1[1] / L_cell);
+        int z = (int) (atom1[2] / L_cell);
+        if (x >= num_cells) {x= num_cells-1;}
+        if (y >= num_cells) {y = num_cells-1;}
+        if (z >= num_cells) {z = num_cells-1;}
+        if (x < 0) {x = 0;}
+        if (y < 0) {y = 0;}
+        if (z < 0) {z = 0;}
+        int cell_index = x + y * num_cells + z * num_cells * num_cells;
+
+        // Loop over neighbouring cells (and current cell)
+        for (int dx = -1; dx <= 1; ++dx) {
+            for (int dy = -1; dy <= 1; ++dy) {
+                for (int dz = -1; dz <= 1; ++dz) {
+
+                    // Calculate coordinates of the cell
+                    int nx = x + dx;
+                    int ny = y + dy;
+                    int nz = z + dz;
+
+                    // Check if neighbouring cell is within the box
+                    if (nx >= 0 && nx < num_cells && ny >= 0 && ny < num_cells && nz >= 0 && nz < num_cells) 
+                    {
+                        // Convert neighbour cell coordinates to linear index
+                        int neighbour_cell_index = nx + ny * num_cells + nz * num_cells * num_cells;
+
+                        // Loop over atoms in current neighbouring cell
+                        for (int j = 0; j < cells[neighbour_cell_index].size(); ++j)
+                        {
+                            // Ensure atom pair haven't already been counted
+                            if (i < cells[neighbour_cell_index][j])
+                            {
+                                // Find position of neighbour atom
+                                std::vector<double> atom2 = positions[cells[neighbour_cell_index][j]];
+
+                                // Increment the count if the atoms are neighbours
+                                if (isNeighbour(atom1, atom2, cutoff)) 
+                                {
+                                    results[i]++;
+                                    results[cells[neighbour_cell_index][j]]++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return results;
+}
+
+// Outputs the statistics
 void printResults(const std::vector<int>& results, int N, double L)
 {
     // Calculate various statistics
@@ -216,7 +316,7 @@ int main(int argc, char **argv)
     // Read in the xyz coordinates
     int N;
     double L;
-    std::vector<std::vector<double>> positions = read_xyz_file("argon147023.xyz", N, L);
+    std::vector<std::vector<double>> positions = read_xyz_file("argon10549.xyz", N, L);
     
     // Define cut-off distance
     double cutoff_distance = 8;
@@ -246,7 +346,7 @@ int main(int argc, char **argv)
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     // Calculate and output results
-    if (rank == 0) std::cout << "Brute Force non-load-balancing approach:" << std::endl;
+    if (rank == 0) std::cout << "Brute Force load-balancing approach:" << std::endl;
     std::vector<int> results1 = findNeighboursBruteForceMPIv2(positions, L, cutoff_distance, rank, num_procs);
     std::vector<int> total_results1(N, 0);
     MPI_Reduce(&results1[0], &total_results1[0], N, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
@@ -276,6 +376,23 @@ int main(int argc, char **argv)
     time_elapsed2 += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
     if (rank == 0) {std::cout << "Time taken (s): " << time_elapsed2 << std::endl;}
     
+    /* Atom-centred cell list load-balanced approach
+    ------------------------------------------*/
+    // Start timer
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    // Calculate and output results
+    if (rank == 0) std::cout << "Atom-centred cell list load-balanced approach:" << std::endl;
+    std::vector<int> results3 = findNeighboursCellListMPIv2(positions, L, cutoff_distance, rank, num_procs);
+    std::vector<int> total_results3(N, 0);
+    MPI_Reduce(&results3[0], &total_results3[0], N, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (rank == 0) {printResults(total_results3, N, L);}
+
+    // End timer
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+    double time_elapsed3 = (finish.tv_sec - start.tv_sec);
+    time_elapsed3 += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+    if (rank == 0) {std::cout << "Time taken (s): " << time_elapsed3 << std::endl;}
 
     // Finalize MPI call
     MPI_Finalize();
